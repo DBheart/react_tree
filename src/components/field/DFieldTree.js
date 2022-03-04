6d1ecd5c4acec4a1d0bce42a14112cc9df9efc62
DBheart <deity98@gmail.com>
2022-03-05 02:30:56 +0900

    코드 맞춤

--- src/components/field/DFieldTree.js
+++ src/components/field/DFieldTree.js
@@ -20,19 +20,17 @@ function DFieldTree({label, name, onChange}) {
   }
 
   return (
-
-      <div style={{display:"flex",width:"100%", position:"relative"}}>
-        {label &&<label htmlFor={name}>{label}</label>}
-        <input readOnly={true} id={name} type={"text"} value={selectNode} onClick={() =>setShow(!show)} />
-        <button onClick={clearNode}><i className={"fas fa-smile fa-xs"}/> </button>
-        {/*<button onClick={clearNode}><i className={"fas fa-smile fa-xs fa-spin"}/> </button>*/}
-        {show &&
-          <div  style={{position:"absolute",left:"50px", top:"25px"}}>
-            <DFilterTree treeData={treeData} handleSelect={handleSelect}/>
-          </div>
-        }
-      </div>
-
+    <div style={{display:"flex",width:"100%", position:"relative"}}>
+      {label &&<label htmlFor={name}>{label}</label>}
+      <input readOnly={true} id={name} type={"text"} value={selectNode} onClick={() =>setShow(!show)} />
+      <button onClick={clearNode}><i className={"fas fa-smile fa-xs"}/> </button>
+      {/*<button onClick={clearNode}><i className={"fas fa-smile fa-xs fa-spin"}/> </button>*/}
+      {show &&
+        <div  style={{position:"absolute",left:"50px", top:"25px"}}>
+          <DFilterTree treeData={treeData} handleSelect={handleSelect}/>
+        </div>
+      }
+    </div>
   );
 }
 
